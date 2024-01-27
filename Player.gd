0d5f838dbc630263b8dd748bbb6d2a44d2000815
Michael Mroz <michaelmroz01111@gmail.com>
2024-01-27 14:18:20 -0400

    add canvas layer for infinite game background

--- Player.gd
+++ Player.gd
@@ -1,29 +1,17 @@
 extends CharacterBody2D
 
 var speed = 400; 
-
-var screen_size; 
-
-func _ready():
-	screen_size = get_viewport_rect().size;
 	
 func _process(delta):
-	var velocity = Vector2.ZERO;
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1;
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1;
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1;
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1;
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * speed;
+	move_and_slide();
+	
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed;
 		$AnimatedSprite2D.play("move");
 	else:
 		$AnimatedSprite2D.play("idle");
-	position += velocity * delta;
-	position = position.clamp(Vector2.ZERO, screen_size);
+	
 	
 
 
