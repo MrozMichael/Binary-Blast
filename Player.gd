0b45f09e58c4fcf8cfa323854d27532e4c9927c0
Michael Mroz <michaelmroz01111@gmail.com>
2024-01-27 15:19:59 -0400

    add borders to game map with collision

--- Player.gd
+++ Player.gd
@@ -6,7 +6,6 @@ func _process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed;
 	move_and_slide();
-	
 	if velocity.length() > 0:
 		$AnimatedSprite2D.play("move");
 	else:
