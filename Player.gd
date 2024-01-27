b89cdcaf75200290d0d747a9b7d55121b8fd7fd1
Michael Mroz <michaelmroz01111@gmail.com>
2024-01-27 11:37:38 -0400

    add collision to player

--- Player.gd
+++ Player.gd
@@ -1,1 +1,26 @@
 extends Area2D
+
+var speed = 400; 
+
+var screen_size; 
+
+func _ready():
+	screen_size = get_viewport_rect().size;
+	
+func _process(delta):
+	var velocity = Vector2.ZERO;
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1;
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1;
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1;
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1;
+	position += velocity * delta;
+	position = position.clamp(Vector2.ZERO, screen_size);
+	
+
+
+	
+	
